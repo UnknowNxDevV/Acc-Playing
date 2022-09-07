27dcd32aa1e354469189e84eb77acea105c498c7
Unknown <108806900+UnknowNxDevV@users.noreply.github.com>
2022-09-07 15:05:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { Client, Collection } = require("discord.js");
 
 
 const config = ("config.json")
+client.login("Token Bot")
+
+
 
 const client = new Client({
   messageCacheLifetime: 60,
@@ -19,10 +22,10 @@ const client = new Client({
     "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
 });
 
-client.on('ready', async () => { console.log(`The`,client.user.tag,`is Online`);})
+client.on('ready', async () => { console.log(The,client.user.tag,is Online);})
 
 client.on('ready', async () => {
 client.user.setPresence({status: "Idle" } )
-client.user.setActivity( {type: "STREAMING", url: "https://github.com/UnknowNxDevV"}, {name: "Under Development By Unknwon "} ) 
+client.user.setActivity( {type: "STREAMING", url: "https://github.com/UnknowNxDevV%22%7D, {name: "Under Development By Unknwon "} ) 
 
    })
