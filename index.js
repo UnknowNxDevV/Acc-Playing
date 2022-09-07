ebabf9efd5cc471889ce70d89684c807875d7f20
Unknown <108806900+UnknowNxDevV@users.noreply.github.com>
2022-09-07 14:57:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const Discord = require("discord.js")
+const { Client, Collection } = require("discord.js");
+
+const client = new Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [
+    "GUILDS",
+    "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
+});
