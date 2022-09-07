b0796dcb8e7e899713e425963cd38b056539304b
Unknown <108806900+UnknowNxDevV@users.noreply.github.com>
2022-09-07 15:00:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,10 @@ const client = new Client({
     "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
 });
 
-client.on('ready', async => {
+client.on('ready', async () => { console.log(`The`,client.user.tag,`is Online`);})
+
+client.on('ready', async () => {
+client.user.setPresence({status: "Idle" } )
+client.user.setActivity( {type: "PLAYING", url: "https://github.com/UnknowNxDevV"}, {name: "Under Development By Unknwon "} ) 
+
+   })
